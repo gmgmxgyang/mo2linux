d895f78693301f2569fcdd1ae8047164007f67b6
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-04-13 08:42:22 +0800

    modify size

--- debian.sh
+++ debian.sh
@@ -448,7 +448,7 @@ GNULINUX() {
 			OSRELEASE="$(cat /etc/os-release | grep -v 'VERSION' | grep 'ID=' | head -n 1 | cut -d '=' -f 2)"
 		fi
 
-		if (whiptail --title "您想要对这个小可爱做什么 " --yes-button "安装工具" --no-button "管理工具" --yesno "检测到您使用的是${OSRELEASE} ${WSL}您是想要启动software安装工具，还是system管理工具？ ♪(^∇^*) " 9 50); then
+		if (whiptail --title "您想要对这个小可爱做什么 " --yes-button "安装工具" --no-button "管理工具" --yesno "检测到您使用的是${OSRELEASE} ${WSL}您是想要启动software安装工具，还是system管理工具？ ♪(^∇^*) " 10 50); then
 			bash -c "$(curl -LfsS 'https://gitee.com/mo2/linux/raw/master/debian-gui-install.bash')"
 			exit 0
 		fi

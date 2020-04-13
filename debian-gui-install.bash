76db91791610f756ae25085074613fde388a91fc
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-04-13 23:43:38 +0800

    startplasma-x11

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -2397,9 +2397,9 @@ FIXVNCdbusLaunch() {
 		elif grep 'startplasma' ~/.vnc/xstartup; then
 			echo "检测您当前的VNC配置为KDE Plasma5，正在将dbus-launch加入至启动脚本中..."
 			sed -i 's/.*startplasma-x11.*/dbus-launch startplasma-x11 \&/' ~/.vnc/xstartup
-			sed -i 's/.*startplasma-x11.*/dbus-launch startplasma-x11 \&/' "/usr/local/bin/startxsdl"
+			sed -i 's/.*startplasma-x11.*/dbus-launch startplasma-x11/' "/usr/local/bin/startxsdl"
 			sed -i 's/.* startkde.*/ dbus-launch startkde \&/' ~/.vnc/xstartup
-			sed -i 's/.* startkde.*/ dbus-launch startkde \&/' "/usr/local/bin/startxsdl"
+			sed -i 's/.* startkde.*/ dbus-launch startkde/' "/usr/local/bin/startxsdl"
 			#sed -i 's/.*startkde.*/dbus-launch startkde \&/' "/usr/local/bin/startxsdl"
 			#sed -i '$ c\dbus-launch startplasma-x11 \&' "/usr/local/bin/startxsdl"
 		elif grep 'gnome-session' ~/.vnc/xstartup; then

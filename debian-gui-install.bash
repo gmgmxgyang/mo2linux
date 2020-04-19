fd0b1a7e300477a7a30d310d20e442029d7cfd68
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-04-19 15:20:41 +0800

    fix centos busybox

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -67,6 +67,10 @@ CHECKdependencies() {
 	if [ ! -e /usr/bin/busybox ] && [ ! -e /bin/busybox ] && [ ! -e /sbin/busybox ]; then
 		if [ "${LINUXDISTRO}" = "gentoo" ]; then
 			dependencies="${dependencies} sys-apps/busybox"
+		elif [ "${LINUXDISTRO}" = "redhat" ]; then
+			if [ "${REDHATDISTRO}" = "fedora" ]; then
+				dependencies="${dependencies} busybox"
+			fi
 		else
 			dependencies="${dependencies} busybox"
 		fi
@@ -85,7 +89,7 @@ CHECKdependencies() {
 				dependencies="${dependencies} catimg"
 			fi
 
-		elif [ "${LINUXDISTRO}" = "redhat" ] || [ "${LINUXDISTRO}" = "arch" ]; then
+		elif [ "${REDHATDISTRO}" = "fedora" ] || [ "${LINUXDISTRO}" = "arch" ]; then
 			dependencies="${dependencies} catimg"
 		fi
 	fi
@@ -250,6 +254,19 @@ CHECKdependencies() {
 			apt install -y ./catimg.deb
 			rm -f catimg.deb
 		fi
+	fi
+
+	if [ ! -e /usr/bin/busybox ] && [ ! -e /usr/local/bin/busybox ]; then
+		cd /tmp
+		wget --no-check-certificate -O ".busybox" "https://gitee.com/mo2/busybox/raw/master/busybox-$(uname -m)"
+		chmod +x .busybox
+		LatestBusyboxDEB="$(curl -L https://mirrors.tuna.tsinghua.edu.cn/debian/pool/main/b/busybox/ | grep static | grep ${archtype} | tail -n 1 | cut -d '=' -f 3 | cut -d '"' -f 2)"
+		wget -O '.busybox.deb' "https://mirrors.tuna.tsinghua.edu.cn/debian/pool/main/b/busybox/${LatestBusyboxDEB}"
+		mkdir -p .busybox-static
+		./.busybox dpkg-deb -X .busybox.deb ./.busybox-static
+		mv -f ./.busybox-static/bin/busybox /usr/local/bin/
+		chmod +x /usr/local/bin/busybox
+		rm -rf .busybox .busybox-static .busybox.deb
 	fi
 
 	if [ "${LINUXDISTRO}" = "debian" ]; then

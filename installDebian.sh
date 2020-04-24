d730c89d826f16cc57bc23e8056ef53d9a1a8d56
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-04-24 18:15:11 +0800

    add arch baidunetdisk

--- installDebian.sh
+++ installDebian.sh
@@ -1543,6 +1543,14 @@ Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch
 EndOfArchMirrors
   fi
   pacman -Syyu --noconfirm
+  if ! grep -q 'archlinuxcn' /etc/pacman.conf; then
+  cat >>/etc/pacman.conf<<-'Endofpacman'
+[archlinuxcn]
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch
+Endofpacman
+  fi
+  pacman -Syu --noconfirm archlinuxcn-keyring
+  pacman -Sy --noconfirm yay
 fi
 #################################
 if [ "$(cat /etc/os-release | grep 'ID=' | head -n 1 | cut -d '=' -f 2)" = "slackware" ]; then

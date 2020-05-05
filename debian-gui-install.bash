5e3cc308288ef44c735df610393ad2fae7d1daa1
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-05-05 17:27:35 +0800

    Add colorful notes

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 curl -Lvo /usr/local/bin/debian-i 'https://gitee.com/mo2/linux/raw/master/tool.sh'
 chmod +x /usr/local/bin/debian-i
+echo "The developer of this tool changed the script name on May 4th, please update it again."
 echo "2020-05-04更换了脚本名称，请再更新一遍"
-echo 'Please update again'

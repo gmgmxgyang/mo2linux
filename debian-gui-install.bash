0c28540d2bc715d37ff0725bc17b3eff4db690b1
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-05-10 18:01:31 +0800

    check curl

--- debian-gui-install.bash
+++ debian-gui-install.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-curl -Lvo /usr/local/bin/debian-i 'https://gitee.com/mo2/linux/raw/master/tool.sh'
+if [ $(command -v curl) ]; then
+    curl -Lvo /usr/local/bin/debian-i 'https://gitee.com/mo2/linux/raw/master/tool.sh'
+else
+    wget -O /usr/local/bin/debian-i 'https://gitee.com/mo2/linux/raw/master/tool.sh'
+fi
 chmod +x /usr/local/bin/debian-i
 echo "The developer of this tool changed the script name on May 4th, please update it again."
 echo "2020-05-04更换了脚本名称，请再更新一遍"

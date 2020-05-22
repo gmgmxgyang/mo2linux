a2c80ea84ebd8bf6fe13a4fe40da416df05ddfd8
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-05-22 13:28:26 +0800

    fix x11passwd

--- tool.sh
+++ tool.sh
@@ -2274,7 +2274,7 @@ configure_x11vnc_remote_desktop_session() {
 		    ${REMOTE_DESKTOP_SESSION_02} &
 		fi
 		#export LANG="zh_CN.UTF8"
-		x11vnc -ncache_cr -xkb -noxrecord -noxfixes -noxdamage -display :233 -forever -bg -rfbauth \${HOME}/.vnc/passwd -users \$(whoami) -rfbport 5901 -noshm &
+		x11vnc -ncache_cr -xkb -noxrecord -noxfixes -noxdamage -display :233 -forever -bg -rfbauth \${HOME}/.vnc/x11passwd -users \$(whoami) -rfbport 5901 -noshm &
 		sleep 2s
 		echo "正在启动x11vnc服务,本机默认vnc地址localhost:5901"
 		echo The LAN VNC address 局域网地址 \$(ip -4 -br -c a | tail -n 1 | cut -d '/' -f 1 | cut -d 'P' -f 2):5901

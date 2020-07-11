908401e4abcac511364a8e34ad48ea378656fbd6
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-07-11 09:26:41 +0800

    move to github

--- .mirror/move.sh
+++ .mirror/move.sh
@@ -1,5 +1,6 @@
 cd ~/gitee/linux-gitee
-cp debian.sh install.sh tool.sh zsh.sh ~/github/github-linux
+cp debian.sh install.sh tool.sh zsh.sh manager.sh ~/github/github-linux
 cd ~/github/github-linux/.mirror
-./github.sh 
-echo 'git commit -am '
+./github.sh
+#echo 'git commit -am '
+code ~/github/github-linux/.mirror

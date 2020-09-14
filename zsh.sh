4e96fb961ce60ef09d8d93c2a56f3c3e1ec419c9
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-09-14 08:49:49 +0800

    stopvnc

--- zsh.sh
+++ zsh.sh
@@ -156,6 +156,7 @@ if [ ! $(command -v debian-i) ]; then
 fi
 chmod +x /usr/local/bin/debian-i
 #########################
+mkdir -p /run/dbus
 rm -rf ${HOME}/.oh-my-zsh
 echo "github.com/ohmyzsh/ohmyzsh"
 git clone --depth=1 https://gitee.com/mirrors/oh-my-zsh.git ${HOME}/.oh-my-zsh || git clone --depth=1 git://github.com/ohmyzsh/ohmyzsh.git ${HOME}/.oh-my-zsh

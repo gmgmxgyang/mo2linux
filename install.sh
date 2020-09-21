a1d4f84c70eb061f23d72c543f9ed60941acffb8
mo2 <5617340+mo2@user.noreply.gitee.com>
2020-09-21 20:11:48 +0800

    alpine zsh

--- install.sh
+++ install.sh
@@ -1833,8 +1833,8 @@ cat >'.profile' <<-'ENDOFbashPROFILE'
 	    fi
 		echo "您已成功安装GNU/Linux,之后可以输${YELLOW}debian${RESET}来进入debian system."
 		bash /usr/local/bin/neofetch
-	    bash zsh.sh
-	    # ash -c "$(wget --no-check-certificate -O- 'https://gitee.com/mo2/zsh/raw/master/zsh.sh')"
+	    exec bash zsh.sh
+		exit 0
 	}
 	########################
 	TMOE_LOCALE_FILE=/usr/local/etc/tmoe-linux/locale.txt
@@ -1887,7 +1887,7 @@ cat >'.profile' <<-'ENDOFbashPROFILE'
 	   apt install -y ^language-pack-${TMOE_LANG_QUATER} 2>/dev/null
 	fi
 
-	echo "您已成功安装GNU/Linux,之后可以输${YELLOW}debian${RESET}来进入debian system."
+	echo "您已成功安装Container,之后可以输${YELLOW}debian${RESET}来进入debian system."
 	echo "Congratulations on your successful installation of GNU/Linux container. After that, you can type debian in termux to enter the container. "
 	echo '正在执行优化步骤，请勿退出!'
 	echo 'Optimization steps are in progress. Do not exit!'

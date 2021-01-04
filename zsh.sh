5163758685bb5e80f15af8dc8a425ac995908540
mo2 <5617340+mo2@user.noreply.gitee.com>
2021-01-04 00:21:01 +0000

    s/enter/type/

--- zsh.sh
+++ zsh.sh
@@ -111,7 +111,7 @@ auto_configure_tmoe_tool_02() {
 }
 auto_check_face_icon() {
 	FACE_ICON_FILE_NAME=$(ls -t ${FACE_ICON_DIR} | egrep '\.jpg|\.png' | head -n 1)
-	cp -v ${FACE_ICON_DIR}/${FACE_ICON_FILE_NAME} ${HOME}/.face
+	cp -vf ${FACE_ICON_DIR}/${FACE_ICON_FILE_NAME} ${HOME}/.face
 	ln -svf ${HOME}/.face ${HOME}/.face.icon
 }
 ################

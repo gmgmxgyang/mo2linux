4b186b1059d0bd2970144a9629082088e50ba962
mo2 <5617340+mo2@user.noreply.gitee.com>
2021-02-27 21:21:35 +0800

    solus + ${DEPENDENCIES}

--- tool.sh
+++ tool.sh
@@ -92,7 +92,7 @@ check_tmoe_command() {
 	else
 		TMOE_TIPS_01="tmoe t"
 	fi
-	TMOE_TIPS_00="Welcome to tmoe linux tool v1.4440,type ${TMOE_TIPS_01} to start this tool."
+	TMOE_TIPS_00="Welcome to tmoe linux tool v1.4442,type ${TMOE_TIPS_01} to start this tool."
 	#勿改00变量
 }
 #########
@@ -614,7 +614,7 @@ check_dependencies() {
 			${TMOE_UPDATE_COMMAND}
 			${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} || ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES}
 			;;
-		arch | gentoo | suse | void) ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} || ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} || ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} ;;
+		arch | gentoo | suse | void | solus) ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} || ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} || ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} ;;
 		redhat)
 			if [ $(command -v dnf) ]; then
 				${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES} || ${TMOE_INSTALLATION_COMMAND} ${DEPENDENCIES}
